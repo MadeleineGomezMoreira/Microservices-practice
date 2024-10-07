837fdf4c977e7d59f86e9856ced8f7089c876468
Clara Madeleine Gómez Moreira <tosigether598@gmail.com>
2024-10-07 13:39:24 +0200

    added POST method for user

--- restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/data/repository/UserRepository.java
+++ restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/data/repository/UserRepository.java
@@ -13,12 +13,13 @@ public class UserRepository {
 
 	
 	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
 	
 	static {
-		users.add(new User(1, "Steven", LocalDate.now().minusYears(30)));
-		users.add(new User(1, "Sarah", LocalDate.now().minusYears(25)));
-		users.add(new User(1, "James", LocalDate.now().minusYears(35)));
-		users.add(new User(1, "Sandra", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Steven", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Sarah", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "James", LocalDate.now().minusYears(35)));
+		users.add(new User(++usersCount, "Sandra", LocalDate.now().minusYears(30)));
 	}
 	
 	public List<User> getAll(){
@@ -27,6 +28,13 @@ public class UserRepository {
 	
 	public User get(int id) {
 		return users.stream().filter(user -> user.getId() == id).findFirst().get();
+	}
+	
+	public void save(User newUser) {
+		newUser.setId(++usersCount);
+		if (!users.contains(newUser)) {
+			users.add(newUser);
+		}
 	}
 	
 }
--- restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/ui/controller/UserController.java
+++ restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/ui/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myproject.rest.webservices.restful_web_services.data.repository.UserRepository;
@@ -24,6 +26,11 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public User get(@PathVariable int id){
 		return repository.get(id);
+	}
+	
+	@PostMapping("/users")
+	public void save(@RequestBody User newUser) {
+		repository.save(newUser);
 	}
 
 
