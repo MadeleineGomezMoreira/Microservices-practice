41842d1b3b6c36dae6ce072b90f7de545864e456
Clara Madeleine Gómez Moreira <tosigether598@gmail.com>
2024-10-07 17:23:13 +0200

    added some argument validations + exception-handling methods

--- restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/data/repository/UserRepository.java
+++ restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/data/repository/UserRepository.java
@@ -6,35 +6,38 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import com.myproject.rest.webservices.restful_web_services.domain.exception.UserNotFoundException;
 import com.myproject.rest.webservices.restful_web_services.domain.model.User;
 
 @Component
 public class UserRepository {
 
-	
 	private static List<User> users = new ArrayList<>();
 	private static int usersCount = 0;
-	
+
 	static {
 		users.add(new User(++usersCount, "Steven", LocalDate.now().minusYears(30)));
 		users.add(new User(++usersCount, "Sarah", LocalDate.now().minusYears(25)));
 		users.add(new User(++usersCount, "James", LocalDate.now().minusYears(35)));
 		users.add(new User(++usersCount, "Sandra", LocalDate.now().minusYears(30)));
 	}
-	
-	public List<User> getAll(){
+
+	public List<User> getAll() {
 		return users;
 	}
-	
+
 	public User get(int id) {
-		return users.stream().filter(user -> user.getId() == id).findFirst().get();
+		return users.stream().filter(user -> user.getId() == id).findFirst().orElseThrow(() -> new UserNotFoundException("User with ID " + id + " not found"));
 	}
-	
-	public void save(User newUser) {
+
+	public User save(User newUser) {
 		newUser.setId(++usersCount);
-		if (!users.contains(newUser)) {
-			users.add(newUser);
-		}
+		users.add(newUser);
+		return newUser;
 	}
 	
+	public void delete(int id) {
+		users.remove(users.stream().filter(user -> user.getId() == id).findFirst().orElseThrow(() -> new UserNotFoundException("User with ID " + id + " not found")));
+	}
+
 }
--- restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/ui/controller/UserController.java
+++ restful-web-services/src/main/java/com/myproject/rest/webservices/restful_web_services/ui/controller/UserController.java
@@ -1,37 +1,54 @@
 package com.myproject.rest.webservices.restful_web_services.ui.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.myproject.rest.webservices.restful_web_services.data.repository.UserRepository;
 import com.myproject.rest.webservices.restful_web_services.domain.model.User;
 
+import jakarta.validation.Valid;
+
+
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	@GetMapping("/users")
-	public List<User> getAll(){
+	public List<User> getAll() {
 		return repository.getAll();
 	}
-	
+
 	@GetMapping("/users/{id}")
-	public User get(@PathVariable int id){
+	public User get(@PathVariable int id) {
 		return repository.get(id);
 	}
-	
-	@PostMapping("/users")
-	public void save(@RequestBody User newUser) {
-		repository.save(newUser);
-	}
 
+	@Validated
+	@PostMapping("/users")
+	public ResponseEntity<User> save(@Valid @RequestBody User newUser) {
+		User saved = repository.save(newUser);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void delete(@PathVariable int id) {
+		repository.delete(id);
+	}
 
 }
